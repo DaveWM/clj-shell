364e2afbb300b911d0e3d17c5b700341f79ae617
David Martin <david.martin@fundingcircle.com>
2019-01-12 16:19:22 +0000

    updated project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-shell "0.1.0-SNAPSHOT"
   :description "Shell-like utility functions for Clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/DaveWM/clj-shell"
   :license {:name "GPL V3"
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]]
